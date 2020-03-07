bf5719cf35587e10c36581a002ad09d1764bcd21
Pranjal Desai <pranjal@fastmodeltechnologies.com>
2020-03-07 05:43:39 -0600

    Initial set up

---
+++ app/src/main/java/com/pranjaldesai/coronavirustracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pranjaldesai.coronavirustracker
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
